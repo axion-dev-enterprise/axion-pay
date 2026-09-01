ea5f59783a07c8bace31b77e0b5eef34bf59529a
AXION Enterprise <axionenterprise777@gmail.com>
2026-09-01 13:37:58 -0300

    chore(frontend): remove inactive payment components

--- webapp/src/components/payment/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './PaymentMethodSelector';
-export * from './CardPaymentForm';
-export * from './PixPaymentDisplay';
-export * from './B2BInvoiceForm';
-export * from './PaymentStatusBadge';
-export * from './StripeCardElement';
-export * from './StylishStripeCheckout';
