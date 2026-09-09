856f085a481226d0bc4491a650ae599ca5d9fd70
AXION Enterprise <axionenterprise777@gmail.com>
2026-09-09 03:07:07 -0300

    [antigravity][axion-pay] fix: restore Tailwind CSS generation on dashboard and docs via @source directives and theme import

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,8 +1,8 @@
+import "./index.css";
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
 const PayDashboard = lazy(() => import("./pages/PayDashboard"));
 const ApiDocs = lazy(() => import("./pages/ApiDocs"));
